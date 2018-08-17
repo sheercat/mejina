db3ceb524a0bbc884062afbebb09b810680ac9a5
Tomoo.Amano <tomoo.amano@linecorp.com>
2018-08-17 11:02:53 +0900

    modify icon

--- mejina/AppDelegate.swift
+++ mejina/AppDelegate.swift
@@ -10,46 +10,45 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var url: String!
     var view: ViewController!
     
     override func awakeFromNib() {
         let aem = NSAppleEventManager.shared()
         aem.setEventHandler(self, andSelector: #selector(self.handleGetURLEvent), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
         
-//        var appUrl = Bundle.main.bundleURL.appendingPathComponent("", isDirectory: true)
-//        var status = LSRegisterURL(appUrl as CFURL!, true)
-
+        //        var appUrl = Bundle.main.bundleURL.appendingPathComponent("", isDirectory: true)
+        //        var status = LSRegisterURL(appUrl as CFURL!, true)
+        
     }
     
     @objc func handleGetURLEvent(_ event: NSAppleEventDescriptor) {
-        NSLog("%@", event)
-//        if (event.eventClass.description == "GURL" && event.eventID.description == "GURL") {
-//            NSLog("%@", event)
-//        }
-
-        url = (event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue)!
-//        let alert = NSAlert()
-//        alert.messageText = url
-//        alert.runModal()
+        //        NSLog("%@", event)
+        //        if (event.eventClass.description == "GURL" && event.eventID.description == "GURL") {
+        //            NSLog("%@", event)
+        //        }
         
-        view.setUrl()
+        let url = (event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue)!
+        //        let alert = NSAlert()
+        //        alert.messageText = url
+        //        alert.runModal()
+        
+        view.setUrl(url)
     }
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         //        let aem = NSAppleEventManager.shared()
-//        aem.setEventHandler(self, andSelector: Selector(("handleGetURLEvent:replyEvent:")), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
+        //        aem.setEventHandler(self, andSelector: Selector(("handleGetURLEvent:replyEvent:")), forEventClass: AEEventClass(kInternetEventClass), andEventID: AEEventID(kAEGetURL))
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         print("bye")
     }
-
+    
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         print("close window")
         return true
     }
-
+    
 }
 
--- mejina/ViewController.swift
+++ mejina/ViewController.swift
@@ -9,47 +9,50 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet weak var text: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         let app = NSApplication.shared.delegate as! AppDelegate
         app.view = self
     }
     
-    func setUrl() {
-        let app = NSApplication.shared.delegate as! AppDelegate
-        text.stringValue = app.url
+    @objc func setUrl(_ url: String) {
+        //        let alert = NSAlert()
+        //        alert.messageText = url
+        //        alert.runModal()
+        //        let app = NSApplication.shared.delegate as! AppDelegate
+        text.stringValue = url
     }
-
+    
     @IBAction func onClick(_ sender: NSButton) {
         if let url = URL(string: text.stringValue) {
-//            let alert = NSAlert()
-//            alert.messageText = url.absoluteString
-//            alert.runModal()
-
+            //            let alert = NSAlert()
+            //            alert.messageText = url.absoluteString
+            //            alert.runModal()
+            
             NSWorkspace.shared.open(url)
             exit(0)
         }
     }
     
-//    @IBAction func done(_ sender: NSTextFieldCell) {
-//        print("done")
-//        if let url = URL(string: sender.stringValue),
-//            NSWorkspace.shared.open(url) {
-//            exit(0)
-//        }
-//    }
+    //    @IBAction func done(_ sender: NSTextFieldCell) {
+    //        print("done")
+    //        if let url = URL(string: sender.stringValue),
+    //            NSWorkspace.shared.open(url) {
+    //            exit(0)
+    //        }
+    //    }
     
-//    override var representedObject: Any? {
-//        didSet {
-//        // Update the view, if already loaded.
-//        }
-//    }
-
-
+    //    override var representedObject: Any? {
+    //        didSet {
+    //        // Update the view, if already loaded.
+    //        }
+    //    }
+    
+    
 }
 
